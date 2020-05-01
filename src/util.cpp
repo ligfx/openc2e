d3962b5914ae474dee2e092b53e4abac9dcd988f
Michael Maltese <michaeljosephmaltese@gmail.com>
2020-05-01 16:41:35 -0700

    util: throw errors on file read problems, add readfile_binary()

--- src/util.cpp
+++ src/util.cpp
@@ -1,12 +1,26 @@
 #include "util.h"
 
 #include <fstream>
+#include <stdexcept>
 
 std::string readfile(const std::string &filename) {
 	std::ifstream in(filename);
+	if (!in) {
+		throw std::runtime_error("Couldn't open \"" + filename + "\"");
+	}
+	in.exceptions(std::ios_base::failbit | std::ios_base::badbit);
 	return readfile(in);
 }
 
 std::string readfile(std::istream &in) {
     return std::string(std::istreambuf_iterator<char>(in), {});
+}
+
+std::vector<unsigned char> readfile_binary(const std::string &filename) {
+	std::ifstream in(filename);
+	if (!in) {
+		throw std::runtime_error("Couldn't open \"" + filename + "\"");
+	}
+	in.exceptions(std::ios_base::failbit | std::ios_base::badbit);
+	return std::vector<unsigned char>(std::istreambuf_iterator<char>(in), {});
 }
