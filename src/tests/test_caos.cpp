b7b47c4973c040831c27af8793d77ff73ca12b26
Michael Maltese <michaeljosephmaltese@gmail.com>
2021-11-14 16:15:56 -0800

    fixme test_caos.cpp when we changed directory structure

--- src/tests/test_caos.cpp
+++ src/tests/test_caos.cpp
@@ -62,48 +62,48 @@ TEST(caos, file) {
 		}
 		~auto_data_directories() { data_directories = original_data_directories; }
 	} auto_directories;
-	run_script("c3", readfile("../tests/file.cos"));
+	run_script("c3", readfile("../src/tests/file.cos"));
 }
 
 TEST(caos, flow) {
-	run_script("c3", readfile("../tests/flow.cos"));
+	run_script("c3", readfile("../src/tests/flow.cos"));
 }
 
 TEST(caos, ifblocks) {
-	run_script("c3", readfile("../tests/ifblocks.cos"));
+	run_script("c3", readfile("../src/tests/ifblocks.cos"));
 }
 
 TEST(caos, parse_comment_end) {
-	run_script("c3", readfile("../tests/parse-comment-end.cos"));
+	run_script("c3", readfile("../src/tests/parse-comment-end.cos"));
 }
 
 TEST(caos, parsing) {
-	run_script("c3", readfile("../tests/parsing.cos"));
+	run_script("c3", readfile("../src/tests/parsing.cos"));
 }
 
 TEST(caos, simpleagent) {
 	auto sprite = Openc2eTestHelper::addBlnkSprite();
-	run_script("c3", readfile("../tests/simpleagent.cos"));
+	run_script("c3", readfile("../src/tests/simpleagent.cos"));
 }
 
 TEST(caos, special_lexing) {
-	run_script("c1", readfile("../tests/special-lexing.cos"));
-	run_script("c2", readfile("../tests/special-lexing.cos"));
+	run_script("c1", readfile("../src/tests/special-lexing.cos"));
+	run_script("c2", readfile("../src/tests/special-lexing.cos"));
 }
 
 TEST(caos, strings) {
-	run_script("c3", readfile("../tests/strings.cos"));
+	run_script("c3", readfile("../src/tests/strings.cos"));
 }
 
 TEST(caos, timeslice) {
-	run_script("c1", readfile("../tests/timeslice.cos"));
-	run_script("c2", readfile("../tests/timeslice.cos"));
+	run_script("c1", readfile("../src/tests/timeslice.cos"));
+	run_script("c2", readfile("../src/tests/timeslice.cos"));
 }
 
 TEST(caos, variables) {
-	run_script("c3", readfile("../tests/variables.cos"));
+	run_script("c3", readfile("../src/tests/variables.cos"));
 }
 
 TEST(caos, vector) {
-	run_script("c3", readfile("../tests/vector.cos"));
+	run_script("c3", readfile("../src/tests/vector.cos"));
 }
